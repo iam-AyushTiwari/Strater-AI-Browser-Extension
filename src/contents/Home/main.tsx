ce8545efc9ddabdf497dded63c2cd321f9d997ca
Ayush Tiwari <141575828+iam-AyushTiwari@users.noreply.github.com>
2025-02-06 13:06:58 +0530

    Revert "Merge pull request #3 from ARYANjoshi09/main"
    This reverts commit 4a883db9f4a99eb12efbe648d27e947efeeeb07d, reversing
    changes made to e2f47d63bd2dbb1a5d58a051e2c0251873b97637.

--- src/contents/Home/main.tsx
+++ src/contents/Home/main.tsx
@@ -1,9 +1,7 @@
 import { StyleProvider } from "@ant-design/cssinjs"
 import { Button, Input, Popover } from "antd"
-import type { GetProps, TreeDataNode, TreeProps } from "antd"
 import Folders from "components/Folders"
 import Providers from "components/Providers"
-import { RootStateProvider } from "contextAPI/RootState"
 import tailwindcss from "data-text:~style.css"
 import antdResetCssText from "data-text:antd/dist/reset.css"
 import type {
@@ -11,7 +9,7 @@ import type {
   PlasmoGetInlineAnchor,
   PlasmoGetShadowHostId
 } from "plasmo"
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { IoMdAdd } from "react-icons/io"
 
 import { Storage } from "@plasmohq/storage"
@@ -41,9 +39,6 @@ const HOST_ID = "custom-sidebar-injected"
 export const getShadowHostId: PlasmoGetShadowHostId = () => HOST_ID
 
 const Main = () => {
-  const [newItem, setNewItem] = useState<string | null>(null)
-  const [buttonClicked, setButtonClicked] = useState<boolean>(false)
-  const [treeData, setTreeData] = useState<TreeDataNode[]>([])
   // useEffect(() => {
   //   const head = document.querySelector("head")
   //   if (head) {
@@ -55,32 +50,39 @@ const Main = () => {
   //   }
   // }, [])
 
-  const handleCreateNewItem = (name: string) => {
-    setNewItem(name) // Pass the new item name to Folders via prop
-    setButtonClicked(true) // Set button clicked to true
-  }
-
-  // useEffect(() => {
-  //  storage.set("treeData",treeData)
-  // },[])
-
-  const [passTreeData, setPassTreeData] = useState<TreeDataNode[]>([])
-
-  // useEffect(() => {
-  //   const fetchTreeData = async () => {
-  //     const data:TreeDataNode[] = await storage.get("treeData");
-  //     setPassTreeData(data);
-  //   };
-  //   fetchTreeData();
-  // }, []);
-
-  console.log("passTreeData", passTreeData)
-
   return (
     <>
       <Providers>
         <StyleProvider container={document.getElementById(HOST_ID).shadowRoot}>
           <div className="py-4 gap-4 w-full">
+            <div className="flex justify-between items-center text-white px-2">
+              <span className="text-2xl">Capsules</span>
+              <Popover
+                zIndex={99999999999999999999}
+                placement="left"
+                content={
+                  <div>
+                    <Input
+                      placeholder="Capsule Name"
+                      style={{
+                        width: 200,
+                        border: "1px solid #ccc",
+                        borderRadius: "5px"
+                      }}
+                    />
+                    <Button type="primary" style={{ marginTop: "10px" }}>
+                      Create
+                    </Button>
+                  </div>
+                }
+                trigger="click">
+                <span className="cursor-pointer p-2 rounded-xl bg-zinc-700 hover:bg-zinc-600">
+                  <IoMdAdd className="text-2xl" />
+                </span>
+              </Popover>
+            </div>
+
+            <Folders />
           </div>
         </StyleProvider>
       </Providers>
